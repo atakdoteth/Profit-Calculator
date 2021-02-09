1a5805f6c4dd8aa7109272f0f11e5b00c5d29439
Atakan Öztürk <thedeini@gmail.com>
2021-02-09 16:42:22 +0300

    Added a 5th method which can calculate the average buy point of an asset with asset values and price points.

--- Profit Calculator/Program.cs
+++ Profit Calculator/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Profit_Calculator
 {
@@ -22,6 +23,7 @@ namespace Profit_Calculator
             Console.WriteLine("2-Calculate Profit with Starting and Percantage Values");
             Console.WriteLine("3-Amount to invest to get the Profit amount with your Percantage Value");
             Console.WriteLine("4-Calculates the Profit with Starting and End Values");
+            Console.WriteLine("5-Calculates average buy point with Amount and Price Points");
             Console.WriteLine();
             Console.Write("What do you want to do?(Write the number):");
             int answer = int.Parse(Console.ReadLine());
@@ -42,7 +44,7 @@ namespace Profit_Calculator
                     calculator.CalculateProfit();
                     break;
                 case 5://Calculates asset average asset value for asset amount
-
+                    calculator.AssetAverage();
                     break;
 
 
@@ -61,12 +63,12 @@ namespace Profit_Calculator
         MenuPrompt:
             Console.Write("Do you want to return to the main menu?(Y/N):");
             string menuPromptAnswer = Console.ReadLine().ToUpper();
-            if(menuPromptAnswer == "Y")//if yes
+            if (menuPromptAnswer == "Y")//if yes
             {
                 Console.WriteLine("Returning back to main menu!");
                 goto MainMenu;
             }
-            else if(menuPromptAnswer == "N")//if no
+            else if (menuPromptAnswer == "N")//if no
             {
                 Console.WriteLine("Have a great day!");
                 Console.ReadLine();
@@ -121,7 +123,7 @@ namespace Profit_Calculator
             float realValue = (start * assetValue) / leverage;
             float profit = realValue * (percantage / 100);
             float end = profit + realValue;
-            
+
 
             Console.WriteLine("Profit is: {0}", profit.ToString("F"));
             Console.WriteLine("End value is: {0}", end.ToString("F"));
@@ -136,7 +138,7 @@ namespace Profit_Calculator
 
             float invest = profit / percantage * 100;
 
-            Console.WriteLine("You need to invest {0} to get {1} with {2} percantage return.",invest,profit,percantage);
+            Console.WriteLine("You need to invest {0} to get {1} with {2} percantage return.", invest, profit, percantage);
         }
 
         //NUMBER 4
@@ -151,7 +153,7 @@ namespace Profit_Calculator
             Console.Write("Asset Value(Use  , instead of . ):");
             float assetValue = float.Parse(Console.ReadLine());
 
-            float percantage = ((end / start) -1);
+            float percantage = ((end / start) - 1);
             float profit = start * assetValue * percantage;
 
             Console.WriteLine("Your profit is -> {0}", profit);
@@ -161,10 +163,43 @@ namespace Profit_Calculator
         //NUMBER 5
         public void AssetAverage()//Calculates average of the asset value with asset amounts and buy prices
         {
+            //Creating vars
+            List<double> amount = new List<double>(), price = new List<double>();//lists to store amounts and price points 
+            double sum = 0, avg=0;//sum of amount and avg price
+
+            Console.Write("How many buy prices there are?:");
+            int buyPriceAmount = int.Parse(Console.ReadLine());
+
+
+            for (int i = 0; i < buyPriceAmount; i++)//Getting and saving prices and amounts
+            {
+                Console.Write("Asset Amount({0}):", i+1);
+                amount.Add(double.Parse(Console.ReadLine()));
+
+                Console.Write("Asset Buy Price({0}):", i+1);
+                price.Add(double.Parse(Console.ReadLine()));
+
+                Console.WriteLine();//Blank space between lines for ease of seeing
+            }
+
+            
+            for(int i = 0; i<amount.Count; i++)//Finding total asset size
+            {
+                Console.WriteLine(amount[i]);
+                sum += amount[i];
+            }
+
+            for (int i = 0; i <price.Count; i++)//calculating avg
+            {
+                avg += (amount[i] / sum) * price[i];
+            }
+
+            Console.WriteLine("Total assets={0}", sum.ToString());
+            Console.WriteLine("Average cost={0}", avg);
+            Console.WriteLine("Total value={0}", sum * avg);
+
+
         }
-
-        
     }
-
 }
 
